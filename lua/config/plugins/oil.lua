5d8605d235a7444134f100a833fa22e85bc6290a
J-Kistner <jk.gm.act@gmail.com>
2025-06-14 12:02:36 -0400

    feat: oil.nvim bindings no longer conflict with harpoon bindings

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -6,7 +6,25 @@ return {
       opts = {
          view_options = {
             show_hidden = true,
-         }
+         },
+         keymaps = {
+            ["<CR>"] = "actions.select",
+            ["<C-p>"] = "actions.preview",
+            ["<C-c>"] = { "actions.close", mode = "n" },
+            ["<C-l>"] = "actions.refresh",
+            ["-"] = { "actions.parent", mode = "n" },
+            ["_"] = { "actions.open_cwd", mode = "n" },
+            ["`"] = { "actions.cd", mode = "n" },
+            ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+            ["gs"] = { "actions.change_sort", mode = "n" },
+            ["gx"] = "actions.open_external",
+            ["g."] = { "actions.toggle_hidden", mode = "n" },
+            ["g\\"] = { "actions.toggle_trash", mode = "n" },
+
+            ["<C-s>"] = {},
+            ["<C-h>"] = {},
+            ["<C-t>"] = {},
+         },
       },
       dependencies = { { "echasnovski/mini.icons", opts = {} }, },
    },
