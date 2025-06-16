42f24da14606c9e1f41689bb813643da56fb718f
J-Kistner <jk.gm.act@gmail.com>
2025-06-16 15:19:26 -0400

    Changed snake case to start with a instead of I

--- lua/config/plugins/autocomplete.lua
+++ lua/config/plugins/autocomplete.lua
@@ -1,27 +1,32 @@
 return {
    {
       "saghen/blink.cmp",
-      dependencies = "rafamadriz/friendly-snippets",
+      dependencies = {
+         "rafamadriz/friendly-snippets",
+      },
       version = "v0.*",
       opts = {
          keymap = {
             preset = "default",
             ["<C-space>"] = { "accept" },
+            ["<C-k>"] = {},
 
          },
          appearance = {
+            highlight_ns = vim.api.nvim_create_namespace('blink_cmp'),
             use_nvim_cmp_as_default = true,
-            nerd_font_variant = "normal",
+            nerd_font_variant = "mono",
          },
          completion = {
             -- ghost_text = { enabled = true },
             menu = { border = 'rounded' },
             documentation = { auto_show = true, window = { border = 'rounded' } },
          },
-         signature = { enabled = true, window = { border = 'rounded' } },
-      },
-      sources = {
-         default = { "lsp", "path", "snippets", "buffer" },
-      },
-   }
+
+         signature = { enabled = true, window = { show_documentation = true, } },
+         sources = {
+            default = { "lsp", "path", "snippets", "buffer", },
+         },
+      }
+   },
 }
--- lua/mconfig/wrap.lua
+++ lua/mconfig/wrap.lua
@@ -1,4 +1,4 @@
-function get_parent()
+function wrap_some()
    local current_node = vim.treesitter.get_node()
    if not current_node then
       return
@@ -10,7 +10,39 @@ function get_parent()
    local buffer = vim.fn.bufnr()
 
    vim.api.nvim_buf_set_text(buffer, srow, scol, srow, scol, { "Some(" });
-   vim.api.nvim_buf_set_text(buffer, erow, ecol + 5, erow, ecol, { ")" });
+   vim.api.nvim_buf_set_text(buffer, erow, ecol + 5, erow, ecol + 5, { ")" });
 end
 
-get_parent()
+function wrap_ok()
+   local current_node = vim.treesitter.get_node()
+   if not current_node then
+      return
+   end
+
+   local srow, scol = current_node:start()
+   local erow, ecol = current_node:end_()
+
+   local buffer = vim.fn.bufnr()
+
+   vim.api.nvim_buf_set_text(buffer, srow, scol, srow, scol, { "Ok(" });
+   vim.api.nvim_buf_set_text(buffer, erow, ecol + 3, erow, ecol + 3, { ")" });
+end
+
+function wrap_err()
+   local current_node = vim.treesitter.get_node()
+   if not current_node then
+      return
+   end
+
+   local srow, scol = current_node:start()
+   local erow, ecol = current_node:end_()
+
+   local buffer = vim.fn.bufnr()
+
+   vim.api.nvim_buf_set_text(buffer, srow, scol, srow, scol, { "Ok(" });
+   vim.api.nvim_buf_set_text(buffer, erow, ecol + 3, erow, ecol + 3, { ")" });
+end
+
+Key("n", "<leader>ws", get_parent, "Wrap current node in Some()")
+Key("n", "<leader>wo", wrap_ok, "Wrap current node in Ok()")
+Key("n", "<leader>we", wrap_err, "Wrap current node in Err()")
