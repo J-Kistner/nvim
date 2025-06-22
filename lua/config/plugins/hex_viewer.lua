1923d42fd1b408cec0ee2beb121a0a04e7a97734
J-Kistner <jk.gm.act@gmail.com>
2025-06-22 11:03:01 -0400

    A lot of random things

--- lua/config/plugins/hex_viewer.lua
+++ lua/config/plugins/hex_viewer.lua
@@ -1,6 +1,10 @@
 return {
    {
       "RaafatTurki/hex.nvim",
-      opts = {}
+      opts = {},
+      config = function()
+         require("hex").setup()
+         vim.api.nvim_create_user_command("HEX", function() require("hex").toggle() end, {})
+      end,
    }
 }
