c343501bf82a0014ac88cf5037f3b75c701dff9b
J-Kistner <jk.gm.act@gmail.com>
2025-06-28 13:14:09 -0400

    Removed unused plugin and opencode file

--- lua/config/plugins/hex_viewer.lua
+++ lua/config/plugins/hex_viewer.lua
@@ -1,10 +1,11 @@
-return {
-   {
-      "RaafatTurki/hex.nvim",
-      opts = {},
-      config = function()
-         require("hex").setup()
-         vim.api.nvim_create_user_command("HEX", function() require("hex").toggle() end, {})
-      end,
-   }
-}
+return {}
+-- return {
+--    {
+--       "RaafatTurki/hex.nvim",
+--       opts = {},
+--       config = function()
+--          require("hex").setup()
+--          vim.api.nvim_create_user_command("HEX", function() require("hex").toggle() end, {})
+--       end,
+--    }
+-- }
