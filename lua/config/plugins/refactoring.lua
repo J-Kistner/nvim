0331d3d80017e33409d9218c911548dd59e14284
J-Kistner <jk.gm.act@gmail.com>
2026-05-03 23:13:37 -0400

    fix: rendermarkdown + ts, colorscheme

--- lua/config/plugins/refactoring.lua
+++ lua/config/plugins/refactoring.lua
@@ -1,48 +1,53 @@
 return {
    {
+      "nvim-lua/plenary.nvim",
+      lazy = false,
+   },
+   {
       "ThePrimeagen/refactoring.nvim",
       dependencies = {
-         "nvim-lua/plenary.nvim",
          "nvim-treesitter/nvim-treesitter",
       },
       lazy = false,
       opts = {},
-      config = function()
-         require('refactoring').setup({
-            prompt_func_return_type = {
-               go = false,
-               java = false,
+       config = function()
+          -- Ensure plenary's async module is available
+          package.loaded['async'] = require('plenary.async')
+          require('refactoring').setup({
+             prompt_func_return_type = {
+                go = false,
+                java = false,
 
-               cpp = false,
-               c = false,
-               h = false,
-               hpp = false,
-               cxx = false,
-            },
-            prompt_func_param_type = {
-               go = false,
-               java = false,
+                cpp = false,
+                c = false,
+                h = false,
+                hpp = false,
+                cxx = false,
+             },
+             prompt_func_param_type = {
+                go = false,
+                java = false,
 
-               cpp = false,
-               c = false,
-               h = false,
-               hpp = false,
-               cxx = false,
-            },
-            printf_statements = {},
-            print_var_statements = {},
-            show_success_message = false, -- shows a message with information about the refactor on success
+                cpp = false,
+                c = false,
+                h = false,
+                hpp = false,
+                cxx = false,
+             },
+             printf_statements = {},
+             print_var_statements = {},
+             show_success_message = false, -- shows a message with information about the refactor on success
 
-         })
+          })
 
-         Key("nvx", "<leader>re", ":Refactor extract", "( Refactor ) Extract")                             -- Extracts highlighted text to new function
-         Key("nvx", "<leader>rf", ":Refactor extract_to_file", "( Refactor ) Extract to File")             -- Extracts to a new function in a new file
-         Key("nvx", "<leader>rv", ":Refactor extract_var", "( Refactor ) Extract Var")                     -- Extracts expression to variable
-         Key("nvx", "<leader>ri", ":Refactor inline_var", "( Refactor ) Inline Var")                       -- Replaces all use of a variable with its definition
-         Key("nvx", "<leader>rI", ":Refactor inline_func", "( Refactor ) Inline Func")                     -- Replaces all uses of a function with its definiton
-         Key("nvx", "<leader>rb", ":Refactor extract_block", "( Refactor ) Extract Block")                 -- No clue yet
-         Key("nvx", "<leader>rB", ":Refactor extract_block_to_file", "( Refactor ) Extract Block to File") -- No clue yet
-      end
+          vim.keymap.set({"n", "v", "x"}, "<leader>re", ":Refactor extract", { desc = "( Refactor ) Extract" })                             -- Extracts highlighted text to new function
+          vim.keymap.set({"n", "v", "x"}, "<leader>rf", ":Refactor extract_to_file", { desc = "( Refactor ) Extract to File" })             -- Extracts to a new function in a new file
+          vim.keymap.set({"n", "v", "x"}, "<leader>rv", ":Refactor extract_var", { desc = "( Refactor ) Extract Var" })                     -- Extracts expression to variable
+          vim.keymap.set({"n", "v", "x"}, "<leader>ri", ":Refactor inline_var", { desc = "( Refactor ) Inline Var" })                       -- Replaces all use of a variable with its definition
+          vim.keymap.set({"n", "v", "x"}, "<leader>rI", ":Refactor inline_func", { desc = "( Refactor ) Inline Func" })                     -- Replaces all uses of a function with its definiton
+          vim.keymap.set({"n", "v", "x"}, "<leader>rb", ":Refactor extract_block", { desc = "( Refactor ) Extract Block" })                 -- No clue yet
+          vim.keymap.set({"n", "v", "x"}, "<leader>rB", ":Refactor extract_block_to_file", { desc = "( Refactor ) Extract Block to File" }) -- No clue yet
+       end
 
    },
 }
